3e2dd5b3c9a09b2c6d38e937e45ce85401ef9493
Ahmad Fajrul Falah <fajar.falachudin@gmail.com>
2022-10-10 12:17:16 +0700

    setup dependencies

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,4 +5,29 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-export 'view/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:money_manager/counter/counter.dart';
+import 'package:money_manager/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: const Color(0xFF13B9FF),
+        ),
+      ),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
+    );
+  }
+}
